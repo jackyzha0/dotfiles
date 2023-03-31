70cfeebfec2a7f4fa96c344564c7bc47f7fc9e94
Jacky Zhao <j.zhao2k19@gmail.com>
2023-03-31 10:48:13 -0700

    packer -> lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,334 +1,325 @@
 local vim = vim
-local execute = vim.api.nvim_command
-local fn = vim.fn
-local bo = vim.bo
 local wo = vim.wo
 local g = vim.g
 local o = vim.o
 vim.opt.termguicolors = true
 
--- ensure that packer is installed
-local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  execute 'packadd packer.nvim'
+-- Vim options
+vim.opt.smartcase = true
+wo.number = true
+g.mouse = "a"
+wo.scrolloff = 8
+wo.sidescrolloff = 8
+wo.wrap = false
+wo.cursorline = true
+o.undodir = "/tmp/.vim-undo-dir"
+o.undofile = true
+o.clipboard = "unnamed"
+g.mapleader = " "
+
+local TAB_WIDTH = 2
+o.tabstop = TAB_WIDTH
+o.shiftwidth = TAB_WIDTH
+o.softtabstop = TAB_WIDTH
+o.expandtab = true
+o.copyindent = true
+o.signcolumn = "yes"
+
+-- bootstrap Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd('packadd packer.nvim')
-
-local packer = require 'packer'
-local util = require 'packer.util'
-
-packer.init({
-  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
-})
-
--- borders and colours
-local border_opts = {
-  border = "rounded",
-}
-
---- startup and add configure plugins
-packer.startup({
-  function()
-    local use = use
-    use {
-      'lewis6991/impatient.nvim',
-      config = function()
-        require 'impatient'
-      end
-    }
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-    use {
-      'nvim-telescope/telescope.nvim', branch = '0.1.x',
-      requires = { { 'nvim-lua/plenary.nvim' } },
-      config = function()
-        require 'telescope'.setup {
-          defaults = {
-            file_ignore_patterns = { ".git/", "node_modules/" },
-            preview = {
-              filesize_hook = function(filepath, bufnr, opts)
-                local max_bytes = 10000
-                local cmd = { "head", "-c", max_bytes, filepath }
-                require('telescope.previewers.utils').job_maker(cmd, bufnr, opts)
-              end
-            }
-          },
+-- plugin setup
+require("lazy").setup({
+  "nvim-tree/nvim-web-devicons",
+  {
+    'navarasu/onedark.nvim',
+    config = function()
+      local onedark = require('onedark')
+      onedark.setup({
+        style = 'cool',
+        transparent = true,
+        code_style = {
+          comments = "none",
+          keywords = "bold"
+        },
+        diagnostics = {
+          darker = false,
+          background = false
+        },
+        colors = {
+          black = "#151820",
+          bg0 = "#242b38",
+          bg1 = "#2d3343",
+          bg2 = "#343e4f",
+          bg3 = "#363c51",
+          bg_d = "#1e242e",
+          bg_blue = "#91E9EE",
+          bg_yellow = "#f0d197",
+          fg = "#dfdee0",
+          purple = "#8E7EB4",
+          green = "#20FFAF",
+          orange = "#db9351",
+          blue = "#0DEFE1",
+          yellow = "#f7c668",
+          cyan = "#07a39a",
+          red = "#F65C5C",
+          grey = "#546178",
+          light_grey = "#7d899f",
+        },
+        highlights = {
+          FloatBorder = { bg = "NONE" },
+          NormalFloat = { bg = "NONE" }
+        },
+        lualine = {
+          transparent = true
+        }
+      })
+      onedark.load()
+    end
+  },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-fzf-native.nvim" },
+    config = function()
+      require 'telescope'.setup {
+        defaults = {
+          file_ignore_patterns = { ".git/", "node_modules/" },
+          preview = {
+            filesize_hook = function(filepath, bufnr, opts)
+              local max_bytes = 10000
+              local cmd = { "head", "-c", max_bytes, filepath }
+              require('telescope.previewers.utils').job_maker(cmd, bufnr, opts)
+            end
+          }
+        },
+        extensions = {
           fzf = {
             fuzzy = true,                   -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true,    -- override the file sorter
             case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
           },
-          pickers = {
-            buffers = {
-              mappings = {
-                i = {
-                  ["<c-x>"] = "delete_buffer",
-                }
+        },
+        pickers = {
+          buffers = {
+            mappings = {
+              i = {
+                ["<c-x>"] = "delete_buffer",
               }
             }
           }
         }
-        require('telescope').load_extension('fzf')
-      end
+      }
+      require('telescope').load_extension('fzf')
+    end
+  },
+  'jghauser/mkdir.nvim',
+  {
+    "utilyre/barbecue.nvim",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      show_modified = true,
+      symbols = {
+        ellipsis = "..."
+      }
     }
-    use {
-      'jghauser/mkdir.nvim'
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        theme = "onedark",
+        icons_enabled = false,
+      }
     }
-    use {
-      'navarasu/onedark.nvim',
-      config = function()
-        local onedark = require('onedark')
-        onedark.setup({
-          style = 'cool',
-          transparent = true,
-          code_style = {
-            comments = "none",
-            keywords = "bold"
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    opts = {
+      show_current_context = true,
+      char = "▏"
+    }
+  },
+  {
+    'junnplus/lsp-setup.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    }
+  },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+    end,
+  },
+  'windwp/nvim-ts-autotag',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    priority = 2000,
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        auto_install = true,
+        autotag = { enable = true },
+        highlight = { enable = true },
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_previous_start = {
+              ["[["] = "@parameter.inner"
+            },
+            goto_next_start = {
+              ["]]"] = "@parameter.inner"
+            }
           },
-          diagnostics = {
-            darker = false,
-            background = false
-          },
-          colors = {
-            black = "#151820",
-            bg0 = "#242b38",
-            bg1 = "#2d3343",
-            bg2 = "#343e4f",
-            bg3 = "#363c51",
-            bg_d = "#1e242e",
-            bg_blue = "#91E9EE",
-            bg_yellow = "#f0d197",
-            fg = "#dfdee0",
-            purple = "#8E7EB4",
-            green = "#20FFAF",
-            orange = "#db9351",
-            blue = "#0DEFE1",
-            yellow = "#f7c668",
-            cyan = "#07a39a",
-            red = "#F65C5C",
-            grey = "#546178",
-            light_grey = "#7d899f",
-          },
-          highlights = {
-            FloatBorder = { bg = "NONE" },
-            NormalFloat = { bg = "NONE" }
-          },
-          lualine = {
-            transparent = true
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["iq"] = "@parameter.inner",
+              ["aq"] = "@parameter.outer",
+              ["ib"] = "@block.inner",
+              ["ab"] = "@block.outer",
+            }
           }
-        })
-        onedark.load()
-      end
+        },
+        disable = function(lang, bufnr)
+          return vim.api.nvim_buf_line_count(bufnr) > 10000
+        end
+      }
+    end
+  },
+  'simrat39/rust-tools.nvim',
+  'antoinemadec/FixCursorHold.nvim',
+  'nvim-treesitter/nvim-treesitter-context',
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  'machakann/vim-highlightedyank',
+  {
+    'mfussenegger/nvim-ts-hint-textobject',
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V',  -- linewise
+              ['@class.outer'] = '<c-v>', -- blockwise
+            },
+          },
+        },
+      }
+    end
+  },
+  {
+    'lewis6991/satellite.nvim',
+    opts = {}
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {}
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
+      'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
+      'onsails/lspkind.nvim'
     }
-    use "nvim-tree/nvim-web-devicons"
-    use({
-      "utilyre/barbecue.nvim",
-      tag = "*",
-      requires = {
-        "SmiteshP/nvim-navic",
-        "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  {
+    "kylechui/nvim-surround",
+    opts = {}
+  },
+  {
+    "windwp/nvim-autopairs",
+    opts = {
+      enable_check_bracket_line = true,
+      check_ts = true
+    }
+  },
+  'lewis6991/gitsigns.nvim',
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require("colorizer").setup({ "*" }, {
+        RGB = true,      -- #RGB hex codes
+        RRGGBB = true,   -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true,   -- CSS rgb() and rgba() functions
+        hsl_fn = true,   -- CSS hsl() and hsla() functions
+        css = false,     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = false,  -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        mode = 'foreground',
+      })
+    end
+  },
+  {
+    'rcarriga/nvim-notify',
+    lazy = true,
+    opts = {
+      fps = 30,
+      stages = "fade_in_slide_out",
+      timeout = 500,
+      top_down = true,
+    },
+  },
+  'rktjmp/highlight-current-n.nvim',
+  'fedepujol/move.nvim',
+  {
+    "stevearc/dressing.nvim",
+    opts = {
+      input = {
+        insert_only = true,
       },
-      after = "nvim-web-devicons",     -- keep this if you're using NvChad
-      config = function()
-        require("barbecue").setup({
-          show_modified = true,
-          symbols = {
-            ellipsis = "..."
-          }
-        })
-      end,
-    })
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'nvim-tree/nvim-web-devicons' },
-      config = function()
-        require('lualine').setup({
-          options = {
-            theme = "onedark",
-            icons_enabled = false,
-          }
-        })
-      end
     }
-    use {
-      'lukas-reineke/indent-blankline.nvim',
-      config = function()
-        require('indent_blankline').setup({
-          show_current_context = true,
-          char = "▏"
-        })
-      end
-    }
-    use {
-      'junnplus/lsp-setup.nvim',
-      requires = {
-        'neovim/nvim-lspconfig',
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-      }
-    }
-    use {
-      "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-      config = function()
-        require("lsp_lines").setup()
-      end,
-    }
-    use 'windwp/nvim-ts-autotag'
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-      config = function()
-        require('nvim-treesitter.configs').setup {
-          auto_install = true,
-          autotag = { enable = true },
-          highlight = { enable = true },
-          textobjects = {
-            move = {
-              enable = true,
-              set_jumps = true,
-              goto_previous_start = {
-                ["[["] = "@parameter.inner"
-              },
-              goto_next_start = {
-                ["]]"] = "@parameter.inner"
-              }
-            },
-            select = {
-              enable = true,
-              lookahead = true,
-              keymaps = {
-                ["iq"] = "@parameter.inner",
-                ["aq"] = "@parameter.outer",
-                ["ib"] = "@block.inner",
-                ["ab"] = "@block.outer",
-              }
-            }
-          },
-          disable = function(lang, bufnr)
-            return vim.api.nvim_buf_line_count(bufnr) > 10000
-          end
-        }
-      end
-    }
-    use 'antoinemadec/FixCursorHold.nvim'
-    use 'nvim-treesitter/nvim-treesitter-context'
-    use 'nvim-treesitter/nvim-treesitter-textobjects'
-    use 'machakann/vim-highlightedyank'
-    use {
-      'mfussenegger/nvim-ts-hint-textobject',
-      config = function()
-        require 'nvim-treesitter.configs'.setup {
-          textobjects = {
-            select = {
-              enable = true,
-              lookahead = true,
-              keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-              },
-              selection_modes = {
-                ['@parameter.outer'] = 'v', -- charwise
-                ['@function.outer'] = 'V',  -- linewise
-                ['@class.outer'] = '<c-v>', -- blockwise
-              },
-            },
-          },
-        }
-      end
-    }
-    use {
-      'lewis6991/satellite.nvim',
-      config = function()
-        require('satellite').setup()
-      end
-    }
-    use {
-      'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup()
-      end
-    }
-    use {
-      "hrsh7th/nvim-cmp",
-      requires = {
-        "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
-        'hrsh7th/cmp-path',
-        'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
-        'onsails/lspkind.nvim'
-      }
-    }
-    use {
-      "kylechui/nvim-surround",
-      config = function()
-        require("nvim-surround").setup()
-      end
-    }
-    use {
-      "windwp/nvim-autopairs",
-      config = function()
-        require("nvim-autopairs").setup {
-          enable_check_bracket_line = true,
-          check_ts = true
-        }
-      end
-    }
-    use {
-      'lewis6991/gitsigns.nvim',
-      config = function()
-        require('gitsigns').setup()
-      end
-    }
-    use {
-      'norcalli/nvim-colorizer.lua',
-      config = function()
-        require("colorizer").setup({ "*" }, {
-          RGB = true,      -- #RGB hex codes
-          RRGGBB = true,   -- #RRGGBB hex codes
-          RRGGBBAA = true, -- #RRGGBBAA hex codes
-          rgb_fn = true,   -- CSS rgb() and rgba() functions
-          hsl_fn = true,   -- CSS hsl() and hsla() functions
-          css = false,     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-          css_fn = false,  -- Enable all CSS *functions*: rgb_fn, hsl_fn
-          mode = 'foreground',
-        })
-      end
-    }
-    use 'rcarriga/nvim-notify'
-    use 'simrat39/rust-tools.nvim'
-    use 'rktjmp/highlight-current-n.nvim'
-    use 'fedepujol/move.nvim'
-    use {
-      "stevearc/dressing.nvim",
-      config = function()
-        require("dressing").setup({
-          input = {
-            insert_only = true,
-          },
-        })
-      end,
-    }
-    use {
-      'akinsho/git-conflict.nvim',
-      tag = "v1.0.0",
-      config = function()
-        require('git-conflict').setup({
-          disable_diagnostics = true
-        })
-      end
-    }
-  end,
-  config = {
-    display = {
-      open_fn = function()
-        return require('packer.util').float(border_opts)
-      end
+  },
+  {
+    'akinsho/git-conflict.nvim',
+    version = "v1.0.0",
+    opts = {
+      disable_diagnostics = true
     }
   }
+}, {
+  lazy = false,
+  version = nil
 })
+
+-- borders and colours
+local border_opts = {
+  border = "rounded",
+}
 
 -- LSP
 require('lsp-setup').setup({
@@ -434,27 +425,6 @@ cmp.setup({
     end, { 'i', 's' }),
   },
 })
-
--- Vim options
-vim.opt.smartcase = true
-wo.number = true
-g.mouse = "a"
-wo.scrolloff = 8
-wo.sidescrolloff = 8
-wo.wrap = false
-wo.cursorline = true
-o.undodir = "/tmp/.vim-undo-dir"
-o.undofile = true
-o.clipboard = "unnamed"
-g.mapleader = " "
-
-local TAB_WIDTH = 2
-o.tabstop = TAB_WIDTH
-o.shiftwidth = TAB_WIDTH
-o.softtabstop = TAB_WIDTH
-o.expandtab = true
-o.copyindent = true
-o.signcolumn = "yes"
 
 -- better diagnostics signs
 local sign = function(opts)
